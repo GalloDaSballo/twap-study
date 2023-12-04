621038031e216060219e3f248b5d012b11843583
Entreprenerd <alex@entreprenerd.xyz>
2023-12-04 18:47:09 +0100

    feat: compare with reasonable caps

--- test/TestCompare.t.sol
+++ test/TestCompare.t.sol
@@ -16,25 +16,29 @@ contract ExampleTwapTest is Test {
         twapOptimized = new OPTIMIZED_RelativeTwapWeightedObserver(0);
     }
 
-
     function _compare() internal {
-      assertEq(twapReference.observe(), twapOptimized.observe(), "observe();");
-      assertEq(twapReference.getRealValue(), twapOptimized.getRealValue(), "getRealValue();");
+        assertEq(twapReference.observe(), twapOptimized.observe(), "observe();");
+        assertEq(twapReference.getRealValue(), twapOptimized.getRealValue(), "getRealValue();");
+        console2.log(twapReference.timeToAccrue(), twapOptimized.timeToAccrue(), "timeToAccrue();");
+        console2.log(twapReference.getRealValue(), twapOptimized.getRealValue(), "getRealValue();");
+        console2.log(twapReference.getRealValue(), twapOptimized.getRealValue(), "getRealValue();");
     }
 
     function testCompare(uint256 entropy, uint128 maxValue) public {
         uint256 startTime = block.timestamp;
         uint256 counter;
         while (entropy > 1_000) {
+            _compare();
             vm.warp(startTime + 1 hours * counter++);
             twapReference.update();
             twapOptimized.update();
             uint256 currentChunk = entropy % 1_000;
 
             // 50% Higher | 50% Lower
-            uint128 val = uint128(currentChunk * uint256(maxValue) / 500);
+            uint128 val = uint128(currentChunk * uint256(maxValue) / 500) % 1e27; // Cap 
 
             twapReference.setValue(val);
+            twapOptimized.setValue(val);
 
             entropy /= 1_000;
         }
